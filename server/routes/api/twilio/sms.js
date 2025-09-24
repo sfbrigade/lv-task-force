02fd802681e64b929096107a382ef4e1061fabcc
Francis Li <francisli@users.noreply.github.com>
2025-09-24 15:38:46 -0700

    [Closes #23] Allows lookup of Large Vehicle eligibility by SMS (#27)
    * Add twilio library
    
    * [Closes #23] Allows lookup of Large Vehicle eligibility by sms
    
    * Add signature validation
    
    * Empty commit to see if Github processing can be triggered

---
+++ server/routes/api/twilio/sms.js
@@ -0,0 +1,50 @@
+import { StatusCodes } from 'http-status-codes';
+import twilio from 'twilio';
+import { z } from 'zod';
+
+export default async function (fastify, opts) {
+  fastify.post('/sms',
+    {
+      schema: {
+        description: 'Receives an SMS message from Twilio.',
+        response: {
+          [StatusCodes.OK]: {
+            content: {
+              'application/xml': {
+                schema: z.string().meta({
+                  description:
+                    'Returns a response in TWIML XML format.',
+                }),
+              },
+            },
+          },
+        },
+      },
+    },
+    async function (request, reply) {
+      const twilioSignature = request.headers['x-twilio-signature'];
+      const twilioUrl = `${process.env.BASE_URL}${request.url}`;
+      const twilioParams = { ...request.body };
+      const isSignatureValid = twilio.validateRequest(process.env.TWILIO_AUTH_TOKEN, twilioSignature, twilioUrl, twilioParams);
+
+      if (!isSignatureValid) {
+        reply.status(StatusCodes.FORBIDDEN).send('Invalid signature');
+        return;
+      }
+
+      const { Body: body } = twilioParams;
+      const licensePlateNumber = body.match(/^[A-Z0-9\- ]{1,10}$/i)?.[0].toUpperCase();
+      const twiml = new twilio.twiml.MessagingResponse();
+      if (licensePlateNumber) {
+        const record = await fastify.prisma.largeVehicle.findFirst({ where: { licensePlateNumber } });
+        if (record) {
+          twiml.message(`License plate: ${licensePlateNumber}. Good news! Your vehicle is in the system. You may qualify for a Large Vehicle Refuge Permit.`);
+        } else {
+          twiml.message(`License plate: ${licensePlateNumber}. We couldn't find your vehicle in the system. Please double-check the license plate number you entered.`);
+        }
+      } else {
+        twiml.message('Invalid license plate number. Please double-check the license plate number you entered.');
+      }
+      reply.type('text/xml').send(twiml.toString());
+    });
+}
