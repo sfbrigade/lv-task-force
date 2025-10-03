4d891f322548634c22e2118b8debcb347ff394db
Francis Li <mail@francisli.com>
2025-10-03 11:40:42 -0700

    Revert server modification for posthog- unnecessary

--- client/src/entry-server.jsx
+++ client/src/entry-server.jsx
@@ -3,7 +3,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { StaticRouter } from 'react-router';
 import { createHead, UnheadProvider, renderSSRHead } from '@unhead/react/server';
-import { PostHogProvider } from 'posthog-js/react';
 
 import { defaultValue } from './StaticContext';
 import StaticContextProvider from './StaticContextProvider';
@@ -44,22 +43,13 @@ export async function render (request, reply, staticContext) {
     env: { ...defaultValue.env, ...staticContext.context.env },
     authContext: { user: request.user?.toJSON() ?? null },
   };
-
-  const posthogOptions = {
-    cookieless_mode: 'always',
-    api_host: staticContext.context.env.VITE_PUBLIC_POSTHOG_HOST,
-    defaults: '2025-05-24',
-  };
-
   const head = createHead();
   const html = ReactDOMServer.renderToString(
     <StaticContextProvider value={staticContext.context}>
       <UnheadProvider value={head}>
-        <PostHogProvider apiKey={staticContext.context.env.VITE_PUBLIC_POSTHOG_KEY} options={posthogOptions}>
-          <StaticRouter location={location}>
-            <App />
-          </StaticRouter>
-        </PostHogProvider>
+        <StaticRouter location={location}>
+          <App />
+        </StaticRouter>
       </UnheadProvider>
     </StaticContextProvider>
   );
